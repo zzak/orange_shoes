32723e07aa7a5f7d555f6c932f8208fcb3a44332
Zachary Scott <zachary.s.scott@gmail.com>
2011-01-20 17:55:20 -0500

    add sample to show off new para method

---
+++ samples/sample5.rb
@@ -0,0 +1,7 @@
+require '../lib/orange_shoes'
+
+Shoes.app interval: 400 do
+  para "Hello, Orange Shoes!", 30, 50 
+  para "From Ash and Zak", 470, 470 
+  image "https://github.com/ashbb/orange_shoes/raw/master/static/oshoes-icon.png", 450, 320, 128, 128
+end
