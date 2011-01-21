de2a5db86aa5ad7d16211af9a0ed5ff3c7424da8
Zachary Scott <zachary.s.scott@gmail.com>
2011-01-21 02:49:06 -0500

    x and y still take precedence over style configuration

--- samples/sample5.rb
+++ samples/sample5.rb
@@ -1,7 +1,7 @@
 require '../lib/orange_shoes'
 
 Shoes.app interval: 400 do
-  para "Hello, Orange Shoes!", "20px", "serif", 30, 50 
-  para "From Ash and Zak", "12px", "serif", 470, 470 
+  para "Hello, Orange Shoes!", 30, 50, "20px", "serif"
+  para "From Ash and Zak", 470, 470, "12px", "serif"
   image "https://github.com/ashbb/orange_shoes/raw/master/static/oshoes-icon.png", 450, 320, 128, 128
 end
