a295e2af0dde0cea38494d3be4f58dfcda20d61f
Zachary Scott <zachary.s.scott@gmail.com>
2011-01-21 00:15:40 -0500

    use new context font support

--- samples/sample5.rb
+++ samples/sample5.rb
@@ -1,7 +1,7 @@
 require '../lib/orange_shoes'
 
 Shoes.app interval: 400 do
-  para "Hello, Orange Shoes!", 30, 50 
-  para "From Ash and Zak", 470, 470 
+  para "Hello, Orange Shoes!", "20px", "serif", 30, 50 
+  para "From Ash and Zak", "12px", "serif", 470, 470 
   image "https://github.com/ashbb/orange_shoes/raw/master/static/oshoes-icon.png", 450, 320, 128, 128
 end
