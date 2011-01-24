89d9d80f999e985fd8d65a7581e8dd825f3d10f6
Zachary Scott <zachary.s.scott@gmail.com>
2011-01-24 13:04:06 -0500

    no animation here

--- samples/sample5.rb
+++ samples/sample5.rb
@@ -1,6 +1,6 @@
 require "orange_shoes"
 
-Shoes.app interval: 400 do
+Shoes.app do
   para "Hello, Orange Shoes!", 30, 50, "20px", "serif"
   para "From Ash and Zak", 470, 470, "12px", "serif"
   image "https://github.com/ashbb/orange_shoes/raw/master/static/oshoes-icon.png", 450, 320, 128, 128
