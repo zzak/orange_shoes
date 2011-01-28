648ae332e73b22007402a8600dcc547270327bf2
Zachary Scott <zachary.s.scott@gmail.com>
2011-01-27 22:26:25 -0500

    clean up whitespace

--- lib/shoes/main.rb
+++ lib/shoes/main.rb
@@ -1,12 +1,8 @@
 class Shoes
-
   def self.app args={}, &blk
     app = App.new args
-
     app.instance_eval &blk
-
     repaint_all_by_order app
-
     make_output_dirs app
   end 
     
@@ -16,7 +12,7 @@ class Shoes
     FileUtils.mkdir_p "#{APP_PATH}/output/static"
     FileUtils.cp_r "#{SRC_DIR}/.", "#{APP_PATH}/output/js/." 
     FileUtils.cp_r "#{STATIC_DIR}/.", "#{APP_PATH}/output/static/." 
-    
+    # write to file  
     output = APP_FILE.gsub('.rb', '.html')
     open "#{APP_PATH}/output/#{output}", 'w' do |f|
       f.puts HEADER % [app.width, app.height], 
